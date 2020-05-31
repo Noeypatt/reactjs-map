c507e3de077d8e6201eab365df64585688e7ef46
noeypatt <s5935512027@phuket.ac.th>
2020-05-31 11:30:35 +0700

    set map center

--- components/Map.js
+++ components/Map.js
@@ -6,7 +6,36 @@ class Map extends Component {
     super(props);
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    const current = this.mapRef.current;
+
+    const map = new maptalks.Map(current, {
+      //Thailand
+      // center: [101.41681, 14.8816],
+      // zoom: 5,
+      // minZoom: 5,
+      // maxZoom: 14,
+
+      //Bangkok
+      center: [100.50709, 13.75847],
+      zoom: 11,
+
+      baseLayer: new maptalks.TileLayer("base", {
+        urlTemplate:
+          "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png",
+        subdomains: ["a", "b", "c", "d", "e"],
+        attribution:
+          '&copy; <a href="http://osm.org">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/">CARTO</a>',
+      }),
+
+      // Marker
+      layers: [
+        new maptalks.VectorLayer("v", [
+          new maptalks.Marker([100.49127, 13.75404]),
+        ]),
+      ],
+    });
+  }
 
   render() {
     return <div />;
