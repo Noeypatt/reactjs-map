3e26adf75555cb6016111627d59014d5c46c0ffc
noeypatt <patnarin241240@gmail.com>
2020-05-31 14:45:47 +0700

    GoogleApi

--- src/components/map-google/Map.js
+++ src/components/map-google/Map.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 import { GoogleApiWrapper } from "google-maps-react";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 
-// ...
-
-class Map extends Component {}
+class Map extends Component {
+  render() {
+    return <div></div>;
+  }
+}
 
 export default GoogleApiWrapper({
   apiKey: YOUR_GOOGLE_API_KEY_GOES_HERE,
