7c3b924d8338e7ed6f74b6f3998add5ede78d6c3
noeypatt <s5935512027@phuket.ac.th>
2020-05-31 11:29:29 +0700

    component/Map

---
+++ components/Map.js
@@ -0,0 +1,16 @@
+import * as maptalks from "maptalks";
+import React, { Component } from "react";
+
+class Map extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return <div />;
+  }
+}
+
+export default Map;
